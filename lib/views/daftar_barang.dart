66ddf291542ed9db074f04dffe1ff2f02e41b5b8
USER <ryansutawijaya@student.ub.ac.id>
2021-07-14 21:24:44 +0700

    DaftarBarangv1

--- lib/views/daftar_barang.dart
+++ lib/views/daftar_barang.dart
@@ -29,8 +29,10 @@ class _DaftarBarangState extends State<DaftarBarang>
 
   @override
   Widget build(BuildContext context) {
+    var w = MediaQuery.of(context);
+
     return Scaffold(
-      backgroundColor: backgroundColor,
+        backgroundColor: backgroundColor,
         floatingActionButton: Padding(
           padding: EdgeInsets.only(
             bottom: 23.0,
@@ -174,6 +176,7 @@ class CardBarang extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var w = MediaQuery.of(context).size.width;
     return new Container(
       child: Card(
           shape:
@@ -194,23 +197,28 @@ class CardBarang extends StatelessWidget {
                         SizedBox(
                           width: 12.0,
                         ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              namaBarang,
-                              style: TextStyle(fontSize: 16.0),
-                            ),
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text(
-                              idBarang,
-                              style: TextStyle(
-                                  fontSize: 10.0, fontWeight: FontWeight.w200),
-                            )
-                          ],
+                        Container(
+                          constraints: BoxConstraints(maxWidth: w * 0.2),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              FittedBox(
+                                child: Text(
+                                  namaBarang,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                idBarang,
+                                style: TextStyle(
+                                    fontSize: 10.0,
+                                    fontWeight: FontWeight.w200),
+                              )
+                            ],
+                          ),
                         ),
                       ],
                     ),
@@ -218,67 +226,41 @@ class CardBarang extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              'Stock',
-                              style: TextStyle(
-                                  fontSize: 14.0, color: primaryColor),
-                            ),
-                            SizedBox(
-                              height: 4.0,
-                            ),
-                            Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  IconButton(
-                                    padding: EdgeInsets.zero,
-                                    constraints: BoxConstraints(),
-                                    onPressed: () {},
-                                    icon:
-                                        Icon(CupertinoIcons.minus_circle_fill),
-                                    color: primaryColor,
-                                    iconSize: 20,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'Stock',
+                                style: TextStyle(
+                                    fontSize: 14.0, color: primaryColor),
+                              ),
+                              SizedBox(
+                                height: 4.0,
+                              ),
+                              Container(
+                                constraints: BoxConstraints(minWidth: 16.0),
+                                child: Text(
+                                  '$jumlah',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 14.0,
                                   ),
-                                  SizedBox(
-                                    width: 12.0,
-                                  ),
-                                  Container(
-                                    constraints: BoxConstraints(minWidth: 16.0),
-                                    child: Text(
-                                      '$jumlah',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        fontSize: 14.0,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: 12.0,
-                                  ),
-                                  IconButton(
-                                    padding: EdgeInsets.zero,
-                                    constraints: BoxConstraints(),
-                                    onPressed: () {},
-                                    icon: Icon(CupertinoIcons.plus_circle_fill),
-                                    color: primaryColor,
-                                    iconSize: 20,
-                                  ),
-                                ])
-                          ],
-                        ),
+                                ),
+                              ),
+                            ]),
                         SizedBox(
-                          width: 12.0,
+                          width: 23.0,
                         ),
                         Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            Text(
-                              'Harga Awal',
-                              style: TextStyle(
-                                fontSize: 14.0,
-                                color: primaryColor,
+                            FittedBox(
+                              fit: BoxFit.contain,
+                              child: Text(
+                                'Harga Awal',
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: primaryColor,
+                                ),
                               ),
                             ),
                             SizedBox(
@@ -290,12 +272,9 @@ class CardBarang extends StatelessWidget {
                                 )),
                           ],
                         ),
-                        SizedBox(
-                          width: 12.0,
-                        ),
                         IconButton(
                             onPressed: () {},
-                            icon: Icon(Icons.arrow_forward_ios_rounded))
+                            icon: Icon(Icons.arrow_forward_ios_rounded),iconSize: 16.0,)
                       ],
                     )
                   ],
