ef8150298890bb571984044a7c8baac489bc7935
USER <ryansutawijaya@student.ub.ac.id>
2021-07-21 19:43:43 +0700

    menghilangkanParameter

--- lib/views/navbar.dart
+++ lib/views/navbar.dart
@@ -10,19 +10,17 @@ import 'profil.dart';
 
 
 class NavBar extends StatefulWidget {
-  final String email;
-  const NavBar(this.email,{Key? key}) : super(key: key);
+
+  const NavBar({Key? key}) : super(key: key);
 
   @override
-  _NavBarState createState() => _NavBarState(email);
+  _NavBarState createState() => _NavBarState();
 }
 
 class _NavBarState extends State<NavBar> {
   late PersistentTabController _controller;
   late bool _hideNavBar;
-  String email;
 
-  _NavBarState(this.email);
 
   @override
   void initState() {
