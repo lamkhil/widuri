5c6cd9df1ce66946c33258c8ed0d5334180f605f
you5096 <62498886+you5096@users.noreply.github.com>
2021-08-08 19:27:04 +0700

    add riwayat transaksi

---
+++ lib/views/Widget/card_riwayat.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:widuri/colors.dart';
+import 'package:widuri/controller/c_transaksi.dart';
+import 'package:widuri/views/Widget/popup_barang.dart';
+
+class CardRiwayat extends StatelessWidget {
+  CardRiwayat(
+      {required this.namaPenjual,
+        required this.tanggal,
+        required this.harga,
+        required this.listBarang,});
+
+  String namaPenjual;
+  String tanggal;
+  int harga;
+  List listBarang;
+  RxInt jumlahTransaksi = 0.obs;
+  @override
+  Widget build(BuildContext context) {
+    var w = MediaQuery.of(context).size.width;
+
+    C_Transaksi transaksiController = C_Transaksi().initialized ? Get.find() : Get.put(C_Transaksi());
+
+    return new Container(
+      child: Card(
+          shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+          child: Padding(
+              padding: EdgeInsets.all(12.0),
+              child: SizedBox(
+                height: 40,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          CupertinoIcons.money_dollar_circle,
+                          color: Colors.amber,
+                        ),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Container(
+                          width: w * 0.275,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              FittedBox(
+                                child: Text(
+                                  namaPenjual,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                harga.toString(),
+                                style: TextStyle(
+                                    fontSize: 10.0,
+                                    fontWeight: FontWeight.w200),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: w * 0.45,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                              Container(
+                                width: w * 0.05,
+                                child: FittedBox(
+                                  child: IconButton(
+                                    onPressed: () {
+
+                                    },
+                                    icon: Icon(Icons.arrow_forward_ios_rounded),
+                                  ),
+                                ),
+                              ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ))),
+    );
+  }
+}
---
+++ lib/views/riwayat_transaksi.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:widuri/colors.dart';
+import 'package:widuri/views/Widget/card_riwayat.dart';
+
+class RiwayatTransaksi extends StatefulWidget {
+  const RiwayatTransaksi({Key? key}) : super(key: key);
+
+  @override
+  _RiwayatTransaksiState createState() => _RiwayatTransaksiState();
+}
+
+class _RiwayatTransaksiState extends State<RiwayatTransaksi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: backgroundColor,
+          title: Text(
+            'Tambah Transaksi',
+            style: TextStyle(color: Colors.black),
+          ),
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+        body: Container(
+            child: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0)),
+                child: new ListTile(
+                  leading: new Icon(Icons.search),
+                  title: new TextField(
+                    autofocus: false,
+                    decoration: new InputDecoration(
+                        hintText: 'Search', border: InputBorder.none),
+                    // onChanged: onSearchTextChanged,
+                  ),
+                  trailing: new IconButton(
+                    icon: new Icon(Icons.filter_list_rounded),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.all(12),
+                  child: Text(
+                    'Daftar Barang',
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'RobotoMono',
+                        color: Colors.black),
+                  )),
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        if (index < 6) {
+                          return CardRiwayat(
+                            namaPenjual: "Lamkhilus",
+                            harga: 100000,
+                            tanggal: "27-Sept-2021",
+                            listBarang: [1, 2, 3],
+                          );
+                        } else {
+                          return SizedBox(
+                            height: 40,
+                          );
+                        }
+                      })),
+              SizedBox(height: 23.0),
+            ],
+          ),
+        )));
+  }
+}
